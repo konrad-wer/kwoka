615fb516b48d37f2c5c316470e0a9156b0bfcbb7
Konrad Werbliński <konrad.werblinski@gmail.com>
2020-01-03 00:13:18 +0100

    Added building initial type enviroment

--- src/ASTBuilder.hs
+++ src/ASTBuilder.hs
@@ -2,7 +2,36 @@ module ASTBuilder where
 
 import AST
 import qualified Data.Map as Map
+import Control.Monad.State
 
 buildEffectEnv :: [TopLevelDef p] -> Map.Map String [ActionDef p]
 buildEffectEnv [] = Map.empty
-buildEffectEnv (DefEff (EffectDef pos name actions) : effs) = Map.insert name actions $ buildEffectEnv effs
+buildEffectEnv (DefEff (EffectDef _ name actions) : defs) = Map.insert name actions $ buildEffectEnv defs
+buildEffectEnv (_ : defs) = buildEffectEnv defs
+
+buildTypeEnv :: [TopLevelDef p] -> Map.Map String TypeScheme
+buildTypeEnv topLevelDefs =
+  flip evalState 0 $ bte topLevelDefs
+  where
+    bte :: [TopLevelDef p] -> State Int (Map.Map String TypeScheme)
+    bte [] = return Map.empty
+    bte (DefEff (EffectDef _ name actions) : defs) = do
+      let names = map (\(ActionDef _ name _) -> name) actions
+      ts <- mapM (buildActionType name) actions
+      res <- bte defs
+      return $ foldr (uncurry Map.insert) res $ zip names ts
+    bte (_ : defs) = bte defs
+
+    buildActionType :: Var -> ActionDef p -> State Int TypeScheme
+    buildActionType effName (ActionDef _ _ args) = do
+      tArgs <- mapM (const (T <$> freshVar)) args
+      tRow <- E <$> freshVar
+      tRes <- T <$> freshVar
+      return $ TypeScheme (tArgs ++ [tRow] ++ [tRes]) $
+        TArrow (TProduct $ map TVar tArgs) (EffLabel effName $ EffVar tRow) $ TVar tRes
+
+    freshVar :: State Int String
+    freshVar = do
+      n <- get
+      modify (+1)
+      return ("$" ++ show n)
