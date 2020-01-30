e4fe56118fc9a4ee28d8661112f587854bbff4ef
Konrad Werbliński <konrad.werblinski@gmail.com>
2020-01-30 16:25:02 +0100

    Added command line arguments parsing and handling

--- app/Main.hs
+++ app/Main.hs
@@ -6,33 +6,49 @@ import Preliminary
 import TypeInference
 import Translate
 import Machine
-import Control.Monad (void)
+import Control.Monad
 import System.Environment
 import Text.Megaparsec.Error (errorBundlePretty)
+import System.Exit
 
+printHelp :: IO ()
+printHelp = putStrLn $ "Usage: kwoka filename [options]\n" ++
+  "Options:\n" ++
+  "-debug - Pretty print program with inferred types to file\n" ++
+  "-help - Print this message\n"
 
-readArgs :: [a] -> Maybe a
-readArgs [] = Nothing
-readArgs xs = return $ head xs
+isArg :: String -> Bool
+isArg ('-' : _) = True
+isArg _ = False
+
+parseArgs :: [String] -> IO (String, [String])
+parseArgs xs = do
+  let filenames = filter (not . isArg) xs
+  let args = filter isArg xs
+  if length filenames /= 1 || any (not . flip elem ["-debug", "-help"]) args then do
+    printHelp
+    exitWith $ ExitFailure 1
+  else
+    return (head filenames, args)
+
 
 main :: IO ()
 main = do
-  args <- readArgs <$> getArgs
-  case args of
-    Nothing -> putStrLn "Please provide filename!"
-    Just filename -> do
-      sourceCode <- readFile filename
-      case parseProgram filename sourceCode of
-        Left err -> putStrLn $ errorBundlePretty err
-        Right ast ->
-          case buildProgram ast of
-            Left err -> print err
-            Right (funs, effectEnv, typeEnv) ->
-              case checkProgram effectEnv typeEnv funs of
-                Left err -> print err
-                Right c -> do
-                  putStrLn $ showTypedProgram c ast
-                  putStrLn "\n\n"
-                  case translateProgram typeEnv funs of
-                    (Nothing, _) -> return ()
-                    (Just prog, env) -> void $ eval prog env [] []
+  (filename, args) <- parseArgs . tail =<< getArgs
+  if "-help" `elem` args then
+    printHelp
+  else do
+    sourceCode <- readFile filename
+    case parseProgram filename sourceCode of
+      Left err -> putStrLn $ errorBundlePretty err
+      Right ast ->
+        case buildProgram ast of
+          Left err -> print err
+          Right (funs, effectEnv, typeEnv) ->
+            case checkProgram effectEnv typeEnv funs of
+              Left err -> print err
+              Right c -> do
+                when("-debug" `elem` args) $ writeFile (filename ++ ".typed") $ showTypedProgram c ast
+                case translateProgram typeEnv funs of
+                  (Nothing, _) -> return ()
+                  (Just prog, env) -> void $ eval prog env [] []
